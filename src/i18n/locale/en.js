f0ed2e69659dac1e5b436213293d4908758bce2f
CJc_3103 <cjc_31032022@163.com>
2022-09-24 22:13:35 +0800

    change file type to .js and update syntax

---
+++ src/i18n/locale/en.js
@@ -0,0 +1,27 @@
+const navbar = {
+    resources: {
+        title: "Resources"
+    },
+    tags: {
+        title: "Tags"
+    },
+    settings: {
+        title: "Settings"
+    }
+}
+const topicMenu = {
+    placeholder: "Input topic name",
+    title: "Select topic",
+    all: "All",
+    noTopic: "No Topic"
+};
+const tagPicker = {
+    placeholder: "Input tag name",
+    pickedTitle: "Picked tags"
+}
+
+export default {
+    navbar,
+    topicMenu,
+    tagPicker,
+}
---
+++ src/i18n/locale/zh-CN.js
@@ -0,0 +1,27 @@
+const navbar = {
+    resources: {
+        title: "资源"
+    },
+    tags: {
+        title: "标签"
+    },
+    settings: {
+        title: "设置"
+    }
+}
+const topicMenu = {
+    placeholder: "输入主题名",
+    title: "选择主题",
+    all: "全部",
+    noTopic: "未分类"
+};
+const tagPicker = {
+    placeholder: "输入标签名",
+    pickedTitle: "已选标签"
+}
+
+export default {
+    navbar,
+    topicMenu,
+    tagPicker,
+}
