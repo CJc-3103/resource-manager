9e0c4a93955a70ad0bd1289a1232337a7faec359
CJc_3103 <cjc_31032022@163.com>
2022-09-24 22:54:57 +0800

    download el-icons module

--- src/main.js
+++ src/main.js
@@ -7,5 +7,10 @@ import store from './store';
 import '@/assets/styles/normalize.scss';
 import '@/assets/styles/index.scss';
 import i18n from '@/i18n';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
-createApp(App).use(ElementPlus).use(store).use(router).use(i18n).mount('#app')
+const app = createApp(App).use(ElementPlus).use(store).use(router).use(i18n);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.mount('#app');
