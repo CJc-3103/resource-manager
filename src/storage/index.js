5d27fbb9767eb67cdf18f5d91a412a01eafa85c3
CJc_3103 <cjc_31032022@163.com>
2022-09-24 20:45:06 +0800

    extract string to variables in module 'localStore', and add 'lang'

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,37 +1,50 @@
 const getItem = (key) => localStorage.getItem(key);
 const setItem = (key, value) => localStorage.setItem(key, value);
 
+const langKey = 'lang';
+const currentNavKey = 'currentNav';
+const currentTopicKey = 'currentTopic';
+const currentTagsKey = 'currentTags';
+
+let lang = {
+    get lang() {
+        return getItem(langKey);
+    },
+    set lang(value) {
+        setItem(langKey, value);
+    },
+}
 let currentNav = {
     get currentNav() {
-        return localStorage.getItem('currentNav');
+        return localStorage.getItem(currentNavKey);
     },
     set currentNav(value) {
-        localStorage.setItem('currentNav', value);
+        localStorage.setItem(currentNavKey, value);
     },
 }
 
 let currentTopic = {
     get currentTopic() {
-        return getItem('currentTopic');
+        return getItem(currentTopicKey);
     },
     set currentTopic(value) {
-        setItem('currentTopic', value);
+        setItem(currentTopicKey, value);
     },
 }
 let currentTags = {
     get currentTags() {
-        return getItem('currentTags');
+        return getItem(currentTags);
     },
     set currentTags(value) {
-        setItem('currentTags', value);
+        setItem(currentTags, value);
     },
 }
 
-
-export default localStore = {
+const localStore = {
+    ...lang,
     ...currentNav,
     ...currentTopic,
     ...currentTags,
 };
 
-// export default currentNav
+export default localStore
