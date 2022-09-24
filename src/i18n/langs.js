19f0123e5a7d48ffeecbecf402c106f1193bde4e
CJc_3103 <cjc_31032022@163.com>
2022-09-24 23:45:10 +0800

    add file langs for language shifting

---
+++ src/i18n/langs.js
@@ -0,0 +1,13 @@
+const langs = [
+    {
+        lang: 'zh-CN',
+        title:
+            '简体中文',
+    },
+    {
+        lang: 'en',
+        title:
+            'English',
+    },
+];
+export default langs
