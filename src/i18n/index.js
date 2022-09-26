0d9180eb9e9d112ed8866da47359748c9da5fb94
CJc_3103 <cjc_31032022@163.com>
2022-09-26 18:50:56 +0800

    refactor: move initalization of i18n to the main module

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,37 +1,33 @@
-import { createI18n } from 'vue-i18n'
 import { zhCN, en } from './locale'
 import el_zhCN from 'element-plus/lib/locale/lang/zh-cn'
 import el_en from 'element-plus/lib/locale/lang/en'
 import store from '@/store'
 
-
-import { recoverFromLocal } from '@/utils/storage/backup';
-recoverFromLocal(); // 刷新后，先从本地备份恢复状态
-
 const defaultLang = 'zhCN'; // defaultLang 的值必须与 messages 对象的属性同名
 const langs = ['zhCN', 'en'];
 const elLocalLangs = { zhCN: el_zhCN, en: el_en };
 
-if (!(store.state.currentLang)) store.commit('setCurrentLang', defaultLang);
-let locale = store.state.currentLang || defaultLang;
+const setupI18nConfig = () => {
+    if (!(store.state.currentLang)) store.commit('setCurrentLang', defaultLang);
+    let locale = store.state.currentLang || defaultLang;
 
-const messages = {
-    zhCN: {
-        ...zhCN,
-        ...el_zhCN,
-    },
-    en: {
-        ...en,
-        ...el_en,
-    },
-};
+    const messages = {
+        zhCN: {
+            ...zhCN,
+            ...el_zhCN,
+        },
+        en: {
+            ...en,
+            ...el_en,
+        },
+    };
 
-const i18n = createI18n({
-    legacy: false,
-    globalInjection: true, // 全局模式，可以直接使用 $t
-    locale,
-    messages,
-});
+    return {
+        legacy: false,
+        globalInjection: true, // 全局模式，可以直接使用 $t
+        locale,
+        messages,
+    };
+}
 
-export default i18n;
-export { langs, elLocalLangs }
+export { langs, elLocalLangs, setupI18nConfig }
--- src/main.js
+++ src/main.js
@@ -4,15 +4,18 @@ import ElementPlus from 'element-plus';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import router from './router';
 import store from './store';
-// import { recoverFromLocal } from '@/utils/storage/backup';
-// recoverFromLocal(); // 刷新后，先从本地备份恢复状态
-
-import i18n from '@/i18n';
+import { createI18n } from 'vue-i18n'
+import { setupI18nConfig } from '@/i18n';
 import App from './App.vue';
 //  样式
 import 'element-plus/dist/index.css';
 import '@/assets/styles/normalize.scss';
 import '@/assets/styles/index.scss';
+
+// 初始化
+import { recoverFromLocal } from '@/utils/storage/backup';
+recoverFromLocal(); // 刷新后，先从本地备份恢复状态
+const i18n = createI18n(setupI18nConfig());
 
 const app = createApp(App).use(ElementPlus, {
     i18n: (key, value) => i18n.t(key, value),
