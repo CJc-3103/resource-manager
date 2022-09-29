ecbd9fff5d5db11b67b3aa603d003074498c6686
CJc_3103 <cjc_31032022@163.com>
2022-09-29 22:39:44 +0800

    update: add tags count in state

--- src/store/modules/resources.js
+++ src/store/modules/resources.js
@@ -4,6 +4,9 @@ export default {
     state: {
         currentTopic: '', // 资源管理页，左侧栏主题导航
         currentTags: [], // 资源管理页，左侧栏主题导航
+        pickedTagsCount: 0,
+    },
+    getters: {
     },
     mutations: {
         setCurrentTopic(state, topic) {
@@ -12,13 +15,46 @@ export default {
         setCurrentTags(state, tags) {
             state.currentTags = tags;
         },
+        setTagInCurrentTags(state, tagId, tagVal) {
+            state.currentTags[tagId] = tagVal;
+        },
+        setPickedTagsCount(state, count) {
+            state.pickedTagsCount = count;
+        },
     },
     actions: {
         updateCurrentTopic({ commit }, topic) {
             commit('setCurrentTopic', topic);
         },
-        updateCurrentTags({ commit }, tags) {
-            commit('setCurrentTags', tags);
+        // initCurrentTags({ commit, state }, tagGroupCount) {
+        //     if (!state.pickedTagsCount) {
+        //         let currentTags = new Array(tagGroupCount).fill('');
+        //         commit('setCurrentTags', currentTags);
+        //     }
+        // },
+        // updateCurrentTags({ commit, dispatch, state }, tagId, tagVal) {
+        //     // 既要负责添加，也要负责修改
+        //     if (!state.currentTags[tagId]) dispatch('incrementPickedTags');
+        //     commit('setTagInCurrentTags', tagId, tagVal);
+        // },
+        removeClosedTag({ commit, dispatch }, tagId) {
+            dispatch('decrementPickedTags');
+            commit('setTagInCurrentTags', tagId, '');
+        },
+        clearCurrentTags({ commit, dispatch, state }) {
+            commit('setPickedTagsCount', 0);
+            // dispatch('initCurrentTags', state.currentTags.length);
+        },
+        incrementPickedTags({ commit, state }) {
+            let count = state.pickedTagsCount + 1;
+            commit('setPickedTagsCount', count);
+        },
+        decrementPickedTags({ commit, state }) {
+            let count = state.pickedTagsCount - 1;
+            commit('setPickedTagsCount', count);
+        },
+        getTag({ state }, id) {
+            return state.currentTags[id];
         },
     },
 };
