4a6cc9e0c9455b76ae7620fc494899e3065304c7
CJc_3103 <cjc_31032022@163.com>
2022-10-05 22:39:16 +0800

    refactor: update css modules structure

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,6 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
-
 <script setup>
 import { ref, watch, computed } from 'vue';
 import { setGlobalStateLocal } from '@/utils/storage/backupState/backupGlobalState';
