b86db8ea85bbb7f42632b1cfe00a1739e259257c
CJc_3103 <cjc_31032022@163.com>
2022-10-05 22:40:30 +0800

    update: delete component from global and move it to view's components

--- src/views/resources/TopicForm.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <el-form :model="form" label-width="120px">
-    <el-form-item label="Activity name">
-      <el-input v-model="form.name" />
-    </el-form-item>
-  </el-form>
-</template>
-
-<script setup></script>
